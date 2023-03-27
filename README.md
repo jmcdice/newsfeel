66d4668c849e395a3753958caa8c7baff59f7d88
Joey McDonald <jmcdice@gmail.com>
2023-03-26 22:51:41 -0600

    Introducing NewsFeel: Understand financial news sentiment

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# newsfeel
+## NewsFeel
+
 NewsFeel: A Sentiment Analysis Tool for Financial News
+
+This repo contains a Python script for analyzing the sentiment of financial news articles. The script uses OpenAI's GPT-3 API to generate a sentiment analysis of each article, and caches the results to improve performance.
+
+## Installation
+
+1. Clone the repo:
+```console
+  git clone https://github.com/username/repo.git
+```
+
+2. Install the required packages:
+```console
+  pip install -r requirements.txt
+```
+
+3. Set up your OpenAI API key as an environment variable:
+```console
+  export OPENAI_API_KEY=your_api_key
+```
+
+## Usage
+The script can be run using the following command:
+```console
+  python3 sentiment_analysis.py [-h] [-n NUM_ARTICLES] [--print_cache] [--analyze_cache]
+```
+
+The optional arguments are:
+
+-n NUM_ARTICLES: the number of articles to process (default is 5)
+--print_cache: print all cached sentiment analysis results
+--analyze_cache: analyze sentiment analysis results in the cache and exit
+When the script is run, it fetches financial news articles from Google News and processes each one using the GPT-3 API. The sentiment analysis results are printed to the console. Results are cached to improve performance, and the cache can be printed or analyzed using the optional arguments.
+
+Note: before running the script, be sure to set up your OpenAI API key as an environment variable.
+
---
+++ newsfeel.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+import requests
+import logging
+import hashlib
+import pickle
+import argparse
+from urllib.parse import urljoin
+from termcolor import colored
+from GoogleNews import GoogleNews
+import openai
+from newspaper import Article
+from urllib.parse import urlparse, urlunparse
+import datetime
+
+logging.getLogger('GoogleNews').setLevel(logging.ERROR)
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def send_query(input, context):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            { "role": "system", "content": context },
+            { "role": "user", "content": input }
+        ],
+        temperature=0.7,
+    )
+    response_text = response['choices'][0]['message']['content'].strip()
+    return response_text
+
+def get_article_content(url, title):
+    url = urljoin("https://news.google.com", url)
+    article = Article(url, language='en')
+    try:
+        article.download()
+        article.parse()
+    except Exception as e:
+        # probably got a subscription error, so just return the title
+        return title
+    return article.text
+
+import datetime
+import hashlib
+import re
+import pickle
+
+def get_cached_sentiment_analysis(url, title, content):
+    if content is None:
+        return "Unknown", 0, None
+    
+    content_hash = hashlib.sha256(content.encode("utf-8")).hexdigest()
+    now = datetime.datetime.now()
+
+    if url in sentiment_cache:
+        cached_time, cached_sentiment, cached_confidence, cached_response, cached_content_hash = sentiment_cache[url]
+        time_diff = now - cached_time
+
+        if time_diff <= expiration_length:
+            print("Article found in cache.")
+            return cached_sentiment, cached_confidence, cached_response
+        else:
+            print("Article found in cache, but expired.")
+
+    context_text = "Analyze the sentiment of this article and rate it as 'bullish', 'very bullish', \
+                   'neutral', 'bearish', or 'very bearish' based on the content. Then print on a line by \
+                   itself: 'Sentiment: <sentiment>' where <sentiment> is the sentiment you chose. \
+                   Print on a line by itself: 'Confidence: <confidence>' where <confidence> is \
+                   a number between 0 and 10 that represents how confident you are in your sentiment choice. \
+                   Then, please provide an explanation for your sentiment choice."
+
+    print("Analyzing article...")
+    try:
+        response = send_query(content, context_text)
+    except Exception as e:
+        response = send_query(content, title)
+
+    sentiment_map = {
+        'very bullish': 'Very Bullish',
+        'bullish': 'Bullish',
+        'neutral': 'Neutral',
+        'unknown': 'Unknown',
+        'bearish': 'Bearish',
+        'very bearish': 'Very Bearish'
+    }
+
+    match = re.search(r'Sentiment:\s*([a-zA-Z\s]+)[^0-9]*\s*Confidence:\s*(\d+)', response, re.IGNORECASE | re.DOTALL)
+
+    if match:
+        sentiment = match.group(1).lower().strip()
+        confidence = int(match.group(2))
+    else:
+        sentiment = "Unknown"
+        confidence = 0
+
+    #sentiment_cache[url] = (now, sentiment, confidence, response, content_hash)
+    sentiment_cache[url] = (now, sentiment_map.get(sentiment.lower(), "Unknown"), confidence, response, content_hash)
+
+
+    # Save cache to disk
+    with open(cache_file, "wb") as f:
+        pickle.dump(sentiment_cache, f)
+
+    return sentiment, confidence, None
+
+def analyze_cache_sentiments():
+    sentiment_mapping = {'bullish': 1, 'neutral': 0, 'bearish': -1}
+    sentiment_sum = 0
+    confidence_sum = 0
+    total_articles = 0
+
+    with open(cache_file, 'rb') as f:
+        sentiment_cache = pickle.load(f)
+
+    for content_hash, (cached_time, cached_sentiment, cached_confidence, cached_response, cached_content_hash) in sentiment_cache.items():
+        sentiment_sum += sentiment_mapping.get(cached_sentiment.lower(), 0)
+        confidence_sum += float(cached_confidence)
+        total_articles += 1
+
+    if total_articles > 0:
+        avg_sentiment = sentiment_sum / total_articles
+        avg_confidence = confidence_sum / total_articles
+
+        sentiment_result = 'Neutral'
+        if avg_sentiment < -1.5:
+            sentiment_result = 'Very Bearish'
+        elif avg_sentiment < -0.5:
+            sentiment_result = 'Bearish'
+        elif avg_sentiment > 0.5:
+            sentiment_result = 'Bullish'
+        elif avg_sentiment > 1.5:
+            sentiment_result = 'Very Bullish'
+
+        print(f'Cache Sentiment Analysis:')
+        print(f'Sentiment: {sentiment_result}')
+        print(f'Average Sentiment: {avg_sentiment}')
+        print(f'Average Confidence: {avg_confidence:.2f}')
+    else:
+        print('No articles found in cache for sentiment analysis.')
+
+def print_cache_info(args):
+    now = datetime.datetime.now()
+    total_articles = len(sentiment_cache)
+    expired_count = 0
+
+    print(f"Total articles in cache: {total_articles}")
+
+    for url, (cached_time, cached_sentiment, cached_confidence, cached_response, cached_content_hash) in sentiment_cache.items():
+        time_diff = now - cached_time
+        if time_diff > expiration_length:
+            expired_count += 1
+        elif args.print_cache:  # Check if --print_cache argument is given
+            print(f"URL: {url}")
+            print(f"Content hash: {cached_content_hash}")
+            print(f"Cached time: {cached_time}")
+            print(f"Sentiment: {cached_sentiment}")
+            print(f"Confidence: {cached_confidence}")
+            print(f"Response: {cached_response}")
+            print("")
+
+    if not args.print_cache:  # Print cache info if --print_cache argument is not given
+        print(f"Total articles in cache: {total_articles}")
+        print(f"Expired articles: {expired_count}")
+        print(f"Non-expired articles: {total_articles - expired_count}")
+
+cache_file = "article_cache.pkl"
+expiration_length = datetime.timedelta(days=1)
+
+try:
+    with open(cache_file, "rb") as f:
+        sentiment_cache = pickle.load(f)
+except FileNotFoundError:
+    sentiment_cache = {}
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Process a specified number of articles.')
+    parser.add_argument('-n', '--num_articles', type=int, default=5, help='Number of articles to process')
+    parser.add_argument('--print_cache', action='store_true', help='Print everything in the cache')
+    parser.add_argument('--analyze_cache', action='store_true', help='Analyze cache sentiments and exit')
+    args = parser.parse_args()
+
+    if args.print_cache:  
+        print_cache_info(args)
+    elif args.analyze_cache:  # Analyze cache sentiments and exit
+        analyze_cache_sentiments()
+        exit()
+    else:  
+        googlenews = GoogleNews(period='1d')
+        googlenews.get_news('Financial News')
+        result = googlenews.result()
+
+        num_articles = args.num_articles if args.num_articles <= len(result) else len(result)
+        print(f"Processing {num_articles} articles...\n")
+
+        for idx, article in enumerate(result[:num_articles]):
+            title = article['title']
+            url = "https://" + article['link']
+            content = get_article_content(url, title)
+            sentiment, confidence, _ = get_cached_sentiment_analysis(url, title, content)
+
+            # Process sentiment analysis results
+            print(f"Article {idx + 1}:")
+            print(f"Title: {title}")
+            print(f"Sentiment: {sentiment}")
+            print("\n")
+
+        # Analyze cache sentiments
+        analyze_cache_sentiments()
+
+        print_cache_info(args)
+
+if __name__ == "__main__":
+    main()
